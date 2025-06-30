2130b2cb150ccaba7f9c2f70cf1fd9e84a25da6a
Raja28 <rajadavid03@gmail.com>
2025-06-30 13:29:53 +0530

    feat: add api to fetch created events

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { event, events, getUserJoinedEvents, participateInEvent } = require("../controllers/user")
+const { event, events, getUserJoinedEvents, participateInEvent, getUserCreatedEvents } = require("../controllers/user")
 const { auth } = require("../middlewares/auth")
 
 router.post("/event", auth, event)
 router.get("/event", auth, events)
 router.get("/event_joined", auth, getUserJoinedEvents)
 router.post("/event_resgister", auth, participateInEvent)
+router.get("/event_created", auth, getUserCreatedEvents)
 
 
 module.exports = router
